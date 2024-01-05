8ced370511ca081101506d80ac7f4132d811e078
jang-namu <ironhee8005@gmail.com>
2024-01-05 15:34:04 +0900

    rename: service -> serivceImpl

--- src/main/java/com/bugflix/weblog/security/CustomUserDetailServiceImpl.java
+++ src/main/java/com/bugflix/weblog/security/CustomUserDetailServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailService implements UserDetailsService {
+public class CustomUserDetailServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
 
