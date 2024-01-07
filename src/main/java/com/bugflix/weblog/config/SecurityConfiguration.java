a404d9c106b4b75e48927cbbfd015c158bb9ffc2
jang-namu <ironhee8005@gmail.com>
2024-01-07 17:42:25 +0900

    feat: springdoc 설정, swagger/회원가입 관련 security path 설정

--- src/main/java/com/bugflix/weblog/config/SecurityConfiguration.java
+++ src/main/java/com/bugflix/weblog/config/SecurityConfiguration.java
@@ -22,7 +22,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.io.IOException;
 import java.util.List;
+
 
 @Configuration
 @EnableWebSecurity
@@ -32,23 +34,27 @@ public class SecurityConfiguration {
     private final JwtProvider jwtProvider;
 
     private static final String[] PERMIT_TO_USER = {
-            "/users/**",
-            "/tasks/**"
     };
 
     private static final String[] SWAGGER_URL_ARRAY = {
-            "/v2/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
             "/swagger-resources",
             "/swagger-resources/**",
             "/configuration/ui",
             "/configuration/security",
-            "/swagger-ui.html",
-            "/webjars/**"
+            "/webjars/**",
+    };
+
+    private static final String[] PERMIT_TO_ALL = {
+            "/api/users",
+            "/api/users/login"
     };
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                // ID, Password 문자열을 Base64로 인코딩하여 전달하는 구조
                 .httpBasic().disable()
                 .csrf().disable()
                 // CORS 설정
@@ -69,13 +75,14 @@ public class SecurityConfiguration {
                 )
                 // Spring Security 세션 정책 : 세션을 생성 및 사용하지 않음
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().authorizeHttpRequests()
+                .and()
                 // 조건별로 요청 허용/제한 설정
-                .requestMatchers("/accounts/**").permitAll()
+                .authorizeRequests()
                 // 회원가입과 로그인은 모두 승인
                 // USER Role이 있는 사용자만 허용
                 .requestMatchers(PERMIT_TO_USER).hasRole("USER")
                 // swagger-ui와 관련된 주소 허용
+                .requestMatchers(PERMIT_TO_ALL).permitAll()
                 .requestMatchers(SWAGGER_URL_ARRAY).permitAll()
                 .anyRequest().denyAll()
                 .and()
@@ -85,7 +92,7 @@ public class SecurityConfiguration {
                 .exceptionHandling()
                 .accessDeniedHandler(new AccessDeniedHandler() {
                     @Override
-                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws ServletException, java.io.IOException {
+                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
                         // 권한 문제가 발생했을 때 이 부분을 호출한다.
                         response.setStatus(403);
                         response.setCharacterEncoding("utf-8");
@@ -95,7 +102,7 @@ public class SecurityConfiguration {
                 })
                 .authenticationEntryPoint(new AuthenticationEntryPoint() {
                     @Override
-                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws ServletException, java.io.IOException {
+                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
                         // 인증문제가 발생했을 때 이 부분을 호출한다.
                         response.setStatus(401);
                         response.setCharacterEncoding("utf-8");
