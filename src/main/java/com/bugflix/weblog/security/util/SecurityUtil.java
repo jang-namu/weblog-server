03445ebb3ef800e3ec7d4a5add402690ca525ff5
jang-namu <ironhee8005@gmail.com>
2024-01-09 21:56:57 +0900

    feat: SecurityUtil 클래스 작성 - SecurityContext에서 Authentication을 통해 username(email)을 얻는 유틸 클래스

---
+++ src/main/java/com/bugflix/weblog/security/util/SecurityUtil.java
@@ -0,0 +1,32 @@
+package com.bugflix.weblog.security.util;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public class SecurityUtil {
+
+    private SecurityUtil() {
+        throw new IllegalStateException("SecurityUtil의 생성자가 호출됐습니다.");
+    }
+
+    public static Optional<String> getCurrentUsername() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null) {
+            return Optional.empty();
+        }
+
+        String username = null;
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
+            username = springSecurityUser.getUsername();
+        } else if (authentication.getPrincipal() instanceof String) {
+            username = (String) authentication.getPrincipal();
+        }
+
+        return Optional.ofNullable(username);
+    }
+}
