cab5e211fa3ac8ebd877df1ed75dbb0d782d758b
1hcoj <chi2941@naver.com>
2024-01-29 19:07:59 +0900

    update-commnet-generatevalue-strategy

--- src/main/java/com/bugflix/weblog/comment/Comment.java
+++ src/main/java/com/bugflix/weblog/comment/Comment.java
@@ -14,7 +14,8 @@ import java.util.List;
 @Getter
 public class Comment {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "comment_id")
     private Long id;
 
