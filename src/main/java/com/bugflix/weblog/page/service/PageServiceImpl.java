2ddabcae9e45afd4e0f77c9bb87a71db6cef6f15
jang-namu <ironhee8005@gmail.com>
2024-02-03 18:36:46 +0900

    remove: 사용되지 않는 메서드 삭제

--- src/main/java/com/bugflix/weblog/page/service/PageServiceImpl.java
+++ src/main/java/com/bugflix/weblog/page/service/PageServiceImpl.java
@@ -1,24 +1,14 @@
 package com.bugflix.weblog.page.service;
 
-import com.bugflix.weblog.page.domain.Page;
 import com.bugflix.weblog.page.repository.PageRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class PageServiceImpl {
     private final PageRepository pageRepository;
-
-    public Page getPage(String url) throws Exception {
-        Optional<Page> pages = pageRepository.findPageByUrl(url);
-        return pages.orElseThrow(() -> {
-            log.debug("jox: not found page ");
-            return new Exception("hello");
-        });
-    }
+    
 }
