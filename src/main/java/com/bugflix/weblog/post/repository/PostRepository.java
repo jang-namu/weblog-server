ce0b5c6756c31718ebda7645c8abeb8f3e2cf937
jang-namu <ironhee8005@gmail.com>
2024-02-04 16:10:03 +0900

    refactor: 코드 정리, 인터페이스 메서드 시그니처에 접근지정자 제거

--- src/main/java/com/bugflix/weblog/post/repository/PostRepository.java
+++ src/main/java/com/bugflix/weblog/post/repository/PostRepository.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    public List<Post> findByPageUrl(String url);
+    List<Post> findByPageUrl(String url);
 
-    public List<Post> findByPageUrlAndUserUserId(String url, Long userId);
+    List<Post> findByPageUrlAndUserUserId(String url, Long userId);
 
 }
