37b3f0fe8b059bfb151cd649985f89c34cf14e3d
jang-namu <ironhee8005@gmail.com>
2024-02-04 16:00:29 +0900

    refactor: PostResponse Data 어노테이션 제거

--- src/main/java/com/bugflix/weblog/post/dto/PostResponse.java
+++ src/main/java/com/bugflix/weblog/post/dto/PostResponse.java
@@ -3,13 +3,16 @@ package com.bugflix.weblog.post.dto;
 import com.bugflix.weblog.post.domain.Post;
 import com.bugflix.weblog.tag.dto.TagResponse;
 import com.bugflix.weblog.user.domain.User;
-import lombok.Data;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-@Data
-@NoArgsConstructor
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PostResponse {
     private Long postId;
     private String title;
@@ -20,20 +23,54 @@ public class PostResponse {
     private boolean isLike;
     private String nickname;
     private String profileImageUrl;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
 
-    public PostResponse(Post post) {
-        title = post.getTitle();
-        content = post.getContent();
-        memo = post.getMemo();
-        postId = post.getPostId();
+    @Builder
+    private PostResponse(Long postId, String title, List<TagResponse> tags, String content, String memo, Long likeCount, boolean isLike, String nickname, String profileImageUrl, LocalDateTime createdDate, LocalDateTime updatedDate) {
+        this.postId = postId;
+        this.title = title;
+        this.tags = tags;
+        this.content = content;
+        this.memo = memo;
+        this.likeCount = likeCount;
+        this.isLike = isLike;
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
     }
 
-    // Todo Profile 추가
-    public PostResponse(Post post, User user, List<TagResponse> tags) {
-        this(post);
-        this.tags = tags;
+    @Builder
+    private PostResponse(Long postId, String title, String content, String memo) {
+        this.postId = postId;
+        this.title = title;
+        this.content = content;
+        this.memo = memo;
+    }
 
-        nickname = user.getNickname();
-        // profileImageUrl = profile.getProfileImageUrl();
+    public static PostResponse of(Post post, User user, List<TagResponse> tags, Long likeCount, Boolean isLike) {
+        return PostResponse.builder()
+                .postId(post.getPostId())
+                .title(post.getTitle())
+                .tags(tags)
+                .content(post.getContent())
+                .memo(post.getMemo())
+                .likeCount(likeCount)
+                .isLike(isLike)
+                .nickname(user.getNickname())
+                .profileImageUrl(user.getProfile().getImageUrl())
+                .createdDate(post.getCreatedDate())
+                .updatedDate(post.getModifiedDate())
+                .build();
+    }
+
+    public static PostResponse from(Post post) {
+        return PostResponse.builder()
+                .postId(post.getPostId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .memo(post.getMemo())
+                .build();
     }
 }
