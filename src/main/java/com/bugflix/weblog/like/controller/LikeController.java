dc6937091700fa0739e197d5e317d8f5782f46b3
1hcoj <chi2941@naver.com>
2024-02-06 20:54:29 +0900

    update-like-swagger

--- src/main/java/com/bugflix/weblog/like/controller/LikeController.java
+++ src/main/java/com/bugflix/weblog/like/controller/LikeController.java
@@ -2,6 +2,8 @@ package com.bugflix.weblog.like.controller;
 
 import com.bugflix.weblog.like.dto.LikeStatusResponse;
 import com.bugflix.weblog.like.service.LikeServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
+@Tag(name = "Like API", description = "좋아요 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -17,6 +20,7 @@ public class LikeController {
     private final LikeServiceImpl likeServiceImpl;
 
     @PatchMapping("/v1/likes/{postId}")
+    @Operation(summary = "좋아요 정보 반환", description = "Post의 좋아요 개수, 나의 좋아요 여부 반환")
     public ResponseEntity<LikeStatusResponse> changeLikeStatus(@PathVariable(name = "postId") Long postId,
                                                                @AuthenticationPrincipal UserDetails userDetails) throws Exception {
         return ResponseEntity.ok(likeServiceImpl.changeLikeStatus(postId, userDetails));
