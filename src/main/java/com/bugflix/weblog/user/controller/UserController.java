61aee8c5eb8cf161c339ee533ea17cc91ba5c3c1
1hcoj <chi2941@naver.com>
2024-04-07 14:05:33 +0900

    update-java-doc-controller

--- src/main/java/com/bugflix/weblog/user/controller/UserController.java
+++ src/main/java/com/bugflix/weblog/user/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
      * 신규 사용자 정보를 등록합니다.
      *
      * @param signUpRequest 신규 등록을 요청한 사용자의 정보;
-     * @return
+     * @return 성공적으로 처리한 경우 성공 코드를 반환합니다.
      */
     @Operation(summary = "회원가입", description = "신규 사용자 정보를 등록합니다.")
     @PostMapping("/v1/users")
@@ -33,6 +33,14 @@ public class UserController {
         return ResponseEntity.ok().build();
     }
 
+    /***
+     * Valid 검사를 통과한 회원 정보를 삭제합니다.
+     *
+     * @param signInRequest 계정 삭제를 요청한 사용자의 정보;
+     *                      삭제 요청이 유효한지 검사하기 위해 Email과 Password를 입력받습니다.
+     * @return 성공적으로 처리한 경우 성공 코드를 반환합니다.
+     * Todo @throws Exception
+     */
     @Operation(summary = "회원탈퇴",
             description = "아이디(email)과 비밀번호 일치여부를 확인하고, 일치하다면 회원 정보를 삭제합니다.")
     @DeleteMapping("/v1/users")
