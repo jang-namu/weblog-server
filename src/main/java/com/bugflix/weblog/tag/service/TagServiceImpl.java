b7d4b7269d83d1c561b4887a52d4bfa172989dc5
jang-namu <ironhee8005@gmail.com>
2024-04-10 23:38:28 +0900

    refactor: 스트림 적용, 가독성 향상

--- src/main/java/com/bugflix/weblog/tag/service/TagServiceImpl.java
+++ src/main/java/com/bugflix/weblog/tag/service/TagServiceImpl.java
@@ -1,14 +1,14 @@
 package com.bugflix.weblog.tag.service;
 
 import com.bugflix.weblog.tag.domain.Tag;
-import com.bugflix.weblog.tag.dto.TagResponse;
 import com.bugflix.weblog.tag.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
@@ -18,12 +18,6 @@ public class TagServiceImpl {
 
     public List<String> findTagsByPostId(Long postId) {
         List<Tag> tags = tagRepository.findTagsByPostPostId(postId);
-        List<String> tagContents = new ArrayList<>();
-
-        tags.forEach(tag -> {
-            tagContents.add(tag.getTagContent());
-        });
-
-        return tagContents;
+        return tags.stream().map(Tag::getTagContent).collect(toList());
     }
 }
