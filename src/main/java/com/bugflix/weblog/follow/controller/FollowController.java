bc10b744803747d632a1651f8011da0ff7337f99
jang-namu <ironhee8005@gmail.com>
2024-04-20 16:12:35 +0900

    docs: FollowController 자바독 작성

--- src/main/java/com/bugflix/weblog/follow/controller/FollowController.java
+++ src/main/java/com/bugflix/weblog/follow/controller/FollowController.java
@@ -5,15 +5,13 @@ import com.bugflix.weblog.follow.dto.FollowResponse;
 import com.bugflix.weblog.follow.service.FollowServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -25,6 +23,13 @@ import java.util.List;
 public class FollowController {
     private final FollowServiceImpl followService;
 
+    /**
+     * 대상 사용자를 팔로잉 목록에 추가
+     *
+     * @param followRequest 팔로잉할 대상 사용자의 nickname
+     * @param userDetails 팔로우 요청한 사용자에 대한 정보;
+     * @return 성공 코드 ( 요청을 성공적으로 처리한 경우 )
+     */
     @PostMapping("/v1/follows")
     @Operation(summary = "following 추가", description = "다른 사용자를 following 목록에 추가합니다.")
     public ResponseEntity<Void> addFollow(@Valid @RequestBody FollowRequest followRequest,
@@ -34,36 +39,62 @@ public class FollowController {
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/v1/follows/following")
-    @Operation(summary = "following 조회", description = "현재 사용자가 following 중인 사용자 목록을 조회합니다.")
-    public ResponseEntity<List<FollowResponse>> searchFollower(@AuthenticationPrincipal UserDetails userDetails){
-
-        return ResponseEntity.ok(followService.searchFollow(userDetails,false));
-    }
-
+    /**
+     * 현재 사용자를 팔로우하는 사용자 리스트 조회
+     *
+     * @param userDetails 팔로우 조회 요청한 사용자에 대한 정보;
+     * @return 팔로워 목록 List
+     */
     @GetMapping("/v1/follows/follower")
     @Operation(summary = "follower 조회", description = "현재 사용자를 follow하는 follower 목록을 조회합니다.")
     public ResponseEntity<List<FollowResponse>> searchFollowing(@AuthenticationPrincipal UserDetails userDetails) {
 
-        return ResponseEntity.ok(followService.searchFollow(userDetails,true));
+        return ResponseEntity.ok(followService.searchFollowers(userDetails));
     }
 
-    @DeleteMapping("/v1/follows/following")
-    @Operation(summary = "following 삭제", description = "following 중인 사용자를 목록에서 삭제")
-    public ResponseEntity<Void> deleteFollowing(@Valid @RequestBody FollowRequest followRequest,
-                                                @AuthenticationPrincipal UserDetails userDetails) {
-        followService.deleteFollow(followRequest,userDetails,true);
+    /**
+     * 현재 사용자가 팔로우하는 사용자 리스트 조회
+     *
+     * @param userDetails 팔로잉 조회 요청한 사용자에 대한 정보;
+     * @return 팔로잉 목록 List
+     */
+    @GetMapping("/v1/follows/following")
+    @Operation(summary = "following 조회", description = "현재 사용자가 following 중인 사용자 목록을 조회합니다.")
+    public ResponseEntity<List<FollowResponse>> searchFollower(@AuthenticationPrincipal UserDetails userDetails){
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(followService.searchFollwings(userDetails));
     }
 
-
+    /**
+     * 사용자 팔로워 목록 중 한 명 삭제
+     *
+     * @param followRequest 삭제할 사용자 ninkname
+     * @param userDetails 팔로우 삭제를 요청한 사용자에 대한 정보;
+     * @return 성공 코드 ( 요청을 성공적으로 처리한 경우 )
+     */
     @DeleteMapping("/v1/follows/follower")
     @Operation(summary = "follower 삭제", description = "사용자를 following하는 follower를 목록에서 삭제")
     public ResponseEntity<Void> deleteFollower(@Valid @RequestBody FollowRequest followRequest,
                                                @AuthenticationPrincipal UserDetails userDetails){
-        followService.deleteFollow(followRequest,userDetails,false);
+        followService.deleteFollower(followRequest,userDetails);
 
         return ResponseEntity.ok().build();
     }
+
+    /**
+     * 사용자가 팔로잉하는 목록 중 한 명 삭제
+     *
+     * @param followRequest 삭제할 사용자 ninkname
+     * @param userDetails 팔로잉 삭제를 요청한 사용자에 대한 정보;
+     * @return 성공 코드 ( 요청을 성공적으로 처리한 경우 )
+     */
+    @DeleteMapping("/v1/follows/following")
+    @Operation(summary = "following 삭제", description = "following 중인 사용자를 목록에서 삭제")
+    public ResponseEntity<Void> deleteFollowing(@Valid @RequestBody FollowRequest followRequest,
+                                                @AuthenticationPrincipal UserDetails userDetails) {
+        followService.deleteFollowing(followRequest,userDetails);
+
+        return ResponseEntity.ok().build();
+    }
+
 }
