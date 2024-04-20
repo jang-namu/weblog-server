b0c10d63aa49ee893b57f3f786bd2c634045ca54
jang-namu <ironhee8005@gmail.com>
2024-04-20 16:03:05 +0900

    refactor: 각 메서드가 서로 분리되도록 수정

--- src/main/java/com/bugflix/weblog/follow/service/FollowServiceImpl.java
+++ src/main/java/com/bugflix/weblog/follow/service/FollowServiceImpl.java
@@ -7,15 +7,13 @@ import com.bugflix.weblog.follow.repository.FollowRepository;
 import com.bugflix.weblog.security.domain.CustomUserDetails;
 import com.bugflix.weblog.user.domain.User;
 import com.bugflix.weblog.user.repository.UserRepository;
-import com.mysema.commons.lang.Pair;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
 
 @Service
 @RequiredArgsConstructor
@@ -25,66 +23,48 @@ public class FollowServiceImpl {
 
 
     public void addFollow(FollowRequest followRequest, UserDetails userDetails) {
-        Pair<User,User> users = searchUser(followRequest,userDetails,true);
-
-        Follow follow = Follow
-                .builder()
-                .follower(users.getFirst())
-                .following(users.getSecond())
-                .build();
-
+        User follower = ((CustomUserDetails) userDetails).getUser();
+        User following = userRepository.findByNickname(followRequest.getNickname()).orElseThrow(()
+                -> new IllegalArgumentException("invalid nickname"));
+        Follow follow = new Follow(follower, following);
         followRepository.save(follow);
     }
 
-    public List<FollowResponse> searchFollow(UserDetails userDetails, boolean isFollower){
+
+    public List<FollowResponse> searchFollowers(UserDetails userDetails) {
         User user = ((CustomUserDetails) userDetails).getUser();
-        List<Follow> follows;
+        List<Follow> follows = followRepository.findByFollowing(user);
+        return follows.stream()
+                .map(Follow::getFollower)
+                .map(follower -> FollowResponse.of(follower, follower.getProfile()))
+                .toList();
+    }
+
+    public List<FollowResponse> searchFollwings(UserDetails userDetails) {
+        User user = ((CustomUserDetails) userDetails).getUser();
+        List<Follow> follows = followRepository.findByFollower(user);
         ArrayList<FollowResponse> followResponses = new ArrayList<>();
 
-        if (isFollower) {
-            follows = followRepository.findByFollowing(user);
-            follows.forEach(follow -> {
-                User follower = follow.getFollower();
-                followResponses.add(FollowResponse.of(follower,follower.getProfile()));
-            });
-        } else {
-            follows = followRepository.findByFollower(user);
-            follows.forEach(follow -> {
-                User following = follow.getFollowing();
-                followResponses.add(FollowResponse.of(following,following.getProfile()));
-            });
-        }
-
-        return followResponses;
+        return follows.stream()
+                .map(Follow::getFollowing)
+                .map(following -> FollowResponse.of(following, following.getProfile()))
+                .toList();
     }
 
-    @Transactional
-    public void deleteFollow(FollowRequest followRequest, UserDetails userDetails, boolean isFollower) {
-        Pair<User,User> users = searchUser(followRequest,userDetails,isFollower);
+    public void deleteFollower(FollowRequest followRequest, UserDetails userDetails) {
+        User user = ((CustomUserDetails) userDetails).getUser();
+        User follower = userRepository.findByNickname(followRequest.getNickname())
+                .orElseThrow(() -> new IllegalArgumentException("invalid nickname"));
 
-        followRepository.deleteByFollowerAndFollowing(users.getFirst(),users.getSecond());
+        followRepository.deleteByFollowerAndFollowing(follower, user);
     }
 
+    public void deleteFollowing(FollowRequest followRequest, UserDetails userDetails) {
+        User user = ((CustomUserDetails) userDetails).getUser();
+        User following = userRepository.findByNickname(followRequest.getNickname())
+                .orElseThrow(() -> new IllegalArgumentException("invalid nickname"));
 
-    /***
-     * User 객체 반환
-     *
-     * @param followRequest 검색을 요청하는 nickname
-     * @param userDetails Login한 사용자 정보
-     * @param isFollower Login한 사용자의 follower 여부
-     * @return first : follower<br>
-     *         second : following
-     */
-    private Pair<User,User> searchUser(FollowRequest followRequest, UserDetails userDetails, boolean isFollower) {
-        User follower,following;
-        if (isFollower){
-            follower = ((CustomUserDetails) userDetails).getUser();;
-            following = userRepository.findByNickname(followRequest.getNickname()).orElseThrow(() -> new IllegalArgumentException("invalid nickname"));
-        } else {
-            follower = userRepository.findByNickname(followRequest.getNickname()).orElseThrow(() -> new IllegalArgumentException("invalid nickname"));
-            following = ((CustomUserDetails) userDetails).getUser();;
-        }
-        return new Pair<>(follower,following);
+        followRepository.deleteByFollowerAndFollowing(user, following);
+
     }
-
 }
