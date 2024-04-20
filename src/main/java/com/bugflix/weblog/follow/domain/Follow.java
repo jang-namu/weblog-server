651564e097004a046c302ae1aef16f856ff4f348
jang-namu <ironhee8005@gmail.com>
2024-04-20 16:05:05 +0900

    fix: 자기 자신을 대상으로한 팔로우 방지 - Follow 클래스에 생성자에 책임을 위임한다.
    음#   design      : CSS 등 사용자 UI 디자인 변경

--- src/main/java/com/bugflix/weblog/follow/domain/Follow.java
+++ src/main/java/com/bugflix/weblog/follow/domain/Follow.java
@@ -8,6 +8,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Entity(name = "follow_tb")
 @Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"follower_id","following_id"})})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -28,6 +30,9 @@ public class Follow extends BaseTimeEntity {
 
     @Builder
     public Follow(User follower, User following){
+        if (Objects.equals(follower.getUserId(), following.getUserId())) {
+            throw new IllegalArgumentException("Can't add self follow");
+        }
         this.follower = follower;
         this.following = following;
     }
