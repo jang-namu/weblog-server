4a0e69cabafd21fd531ff13605cbe7f5f5ff7278
jang-namu <ironhee8005@gmail.com>
2024-04-20 16:04:23 +0900

    fix: 서비스의 Transactional을 레포지터리로 옮김 - 레포지터리에 '미리 선언되어 있지 않은' 메서드를 정의할 경우, 기본 Transactional이 제공되지 않음 - deleteById와 같이 미리 선언되어 있는 메서드의 구현은 이미 Transactional이 붙어있는 것을 알 수 있음

--- src/main/java/com/bugflix/weblog/follow/repository/FollowRepository.java
+++ src/main/java/com/bugflix/weblog/follow/repository/FollowRepository.java
@@ -3,14 +3,15 @@ package com.bugflix.weblog.follow.repository;
 import com.bugflix.weblog.follow.domain.Follow;
 import com.bugflix.weblog.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface FollowRepository  extends JpaRepository<Follow, Long> {
 
     public List<Follow> findByFollower(User follower);
     public List<Follow> findByFollowing(User following);
+    @Transactional
     public void deleteByFollowerAndFollowing(User follower, User following);
 
 }
