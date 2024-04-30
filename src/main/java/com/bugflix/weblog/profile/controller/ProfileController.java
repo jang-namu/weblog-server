c3081607dcf0e2a80853b005d6450d1c4f8c54dc
jang-namu <ironhee8005@gmail.com>
2024-04-30 21:13:26 +0900

    feat: 다른 사람 프로필 조회

--- src/main/java/com/bugflix/weblog/profile/controller/ProfileController.java
+++ src/main/java/com/bugflix/weblog/profile/controller/ProfileController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,9 +30,21 @@ public class ProfileController {
      * @return 현재 사용자의 Profile
      */
     @GetMapping("/v1/profiles/mine")
-    @Operation(summary = "Profile 조회", description = "사용자 본인 Profile을 조회합니다.")
+    @Operation(summary = "마이프로필 조회", description = "사용자 본인 프로필을 조회합니다.")
     public ResponseEntity<ProfileResponse> getMyProfile(@AuthenticationPrincipal UserDetails userDetails) {
         return ResponseEntity.ok(profileService.getMyProfile(userDetails));
+    }
+
+    /***
+     * 다른 사람의 Profile을 조회합니다.
+     *
+     * @param nickname 검색할 사용자 이름
+     * @return 현재 사용자의 Profile
+     */
+    @GetMapping("/v1/profiles/users/{nickname}")
+    @Operation(summary = "다른 사람 프로필 조회", description = "다른 사람의 프로필을 조회합니다.")
+    public ResponseEntity<ProfileResponse> getOthersProfile(@PathVariable String nickname) {
+        return ResponseEntity.ok(profileService.getOthersProfile(nickname));
     }
 
 }
--- src/main/java/com/bugflix/weblog/profile/service/ProfileServiceImpl.java
+++ src/main/java/com/bugflix/weblog/profile/service/ProfileServiceImpl.java
@@ -7,6 +7,7 @@ import com.bugflix.weblog.profile.dto.ProfileResponse;
 import com.bugflix.weblog.profile.repository.ProfileRepository;
 import com.bugflix.weblog.security.domain.CustomUserDetails;
 import com.bugflix.weblog.user.domain.User;
+import com.bugflix.weblog.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,12 +18,21 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class ProfileServiceImpl {
     private final ProfileRepository profileRepository;
+    private final UserRepository userRepository;
 
     /**
      * 나의 Profile 반환
      */
     public ProfileResponse getMyProfile(UserDetails userDetails) {
         User user = ((CustomUserDetails) userDetails).getUser();
+        Profile profile = profileRepository.findByUserUserId(user.getUserId())
+                .orElseThrow(() -> new ResourceNotFoundException(Errors.PROFILE_NOT_FOUND));
+        return ProfileResponse.of(user, profile);
+    }
+
+    public ProfileResponse getOthersProfile(String nickname) {
+        User user = userRepository.findByNickname(nickname)
+                .orElseThrow(() -> new ResourceNotFoundException(Errors.USER_NOT_FOUND));
         Profile profile = profileRepository.findByUserUserId(user.getUserId())
                 .orElseThrow(() -> new ResourceNotFoundException(Errors.PROFILE_NOT_FOUND));
         return ProfileResponse.of(user, profile);
