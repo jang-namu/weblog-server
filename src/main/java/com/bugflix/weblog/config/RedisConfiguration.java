d17c685598b2faec4379d072e322b7b837c9ee84
jang-namu <ironhee8005@gmail.com>
2024-05-18 17:30:16 +0900

    chore: Redis Config 변경 - NotifcationResponse 직렬화를 위한 Serializer

--- src/main/java/com/bugflix/weblog/config/RedisConfiguration.java
+++ src/main/java/com/bugflix/weblog/config/RedisConfiguration.java
@@ -1,11 +1,15 @@
 package com.bugflix.weblog.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -19,21 +23,24 @@ public class RedisConfiguration {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
-        // redisTemplate를 받아와서 set, get, delete를 사용
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        // setKeySerializer, setValueSerializer 설정
-        // redis-cli을 통해 직접 데이터를 조회 시 알아볼 수 없는 형태로 출력되는 것을 방지
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
         redisTemplate.setConnectionFactory(redisConnectionFactory());
+        // setKeySerializer, setValueSerializer 설정, redis-cli을 통해 직접 데이터를 조회 시 알아볼 수 없는 형태로 출력되는 것을 방지
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
+        Jackson2JsonRedisSerializer<Object> valueSerializer = new
+                Jackson2JsonRedisSerializer<>(mapper, Object.class);
+        redisTemplate.setValueSerializer(valueSerializer);
 
         return redisTemplate;
     }
-
-
 }
